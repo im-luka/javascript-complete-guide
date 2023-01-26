b2e641684673e8eb2bf3946c0adff25c44531a6d
im-luka <lux.9833@gmail.com>
2023-01-26 21:49:35 +0100

    01: values & variables

--- 01 - javascript fundamentals 01/script.js
+++ 01 - javascript fundamentals 01/script.js
@@ -1,4 +1,30 @@
+// ⬇️ Getting to know JS
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
-
+if (js === "amazing") console.log("Javascript is FUN!");
 console.log(40 + 80 + 23 - 10);
+
+// ⬇️ Values & variables
+console.log("Luka");
+console.log(23);
+
+// some of the naming conventions
+let firstName = "Luka";
+let first = "lux";
+let firstNamePerson;
+let first_name;
+
+// letters, numbers, underscores or dollar symbols are used for naming
+let lux_dule = "lux dule";
+let $function = 27;
+
+let person = "John";
+let PI = 3.1415;
+
+// don't start with uppercase letter
+// keep variable names descriptive
+let myFirstJob = "Developer";
+let myCurrentJob = "Teacher";
+
+console.log(firstName);
+console.log(person);
+console.log(myCurrentJob);
