2c8e019c484c95a8ad6187fa7a1406bbe489a8b6
im-luka <lux.9833@gmail.com>
2023-01-28 18:56:03 +0100

    02: arrow functions

--- 02 - javascript fundamentals 02/script.js
+++ 02 - javascript fundamentals 02/script.js
@@ -56,3 +56,17 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2);
+
+// ⬇️ Arrow Functions
+// (parameter) => return value
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1995);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1998, "john"));
+console.log(yearsUntilRetirement(1989, "bob"));
