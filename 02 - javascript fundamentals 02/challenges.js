e50942160a05a0b2ece4bd2c746315b96fe46c72
im-luka <lux.9833@gmail.com>
2023-01-28 20:03:41 +0100

    02: challenge #2

--- 02 - javascript fundamentals 02/challenges.js
+++ 02 - javascript fundamentals 02/challenges.js
@@ -1,4 +1,4 @@
-// CODE CHALLENGE #1
+// CODING CHALLENGE #1
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
@@ -16,3 +16,23 @@ console.log("dolphins average", dolphinsAverage);
 console.log("koalas average", koalasAverage);
 
 console.log(checkWinner(dolphinsAverage, koalasAverage));
+
+// CODING CHALLENGE #2
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const calcTotal = (bill, tip) => bill + tip;
+
+const bills = [125, 777, 44];
+const tips = [
+  calcTip(bills[0]),
+  calcTip(bills[1]),
+  calcTip(bills[bills.length - 1]),
+];
+const totals = [
+  calcTotal(bills[0], tips[0]),
+  calcTotal(bills[1], tips[1]),
+  calcTotal(bills[2], tips[2]),
+];
+console.log(bills, tips, totals);
