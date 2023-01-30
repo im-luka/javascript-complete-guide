02d9a0f71da7d31e030316cac1a5549e688d001a
im-luka <lux.9833@gmail.com>
2023-01-30 19:55:59 +0100

    03: setting up prettier & vscode

---
+++ 03 - developer skills and editor setup/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Prettier - the best extension for formatting your code
+// custom configuration in .prettierrc file
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+
+// global snippets file -> create snippets as you want
+
+// TODO: highlight extension - great support & customization ability
