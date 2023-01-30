700c803facc4471e6de469ca614c10465ce2995d
im-luka <lux.9833@gmail.com>
2023-01-30 20:03:06 +0100

    03: installing node.js & setting up dev env

--- 03 - developer skills and editor setup/script.js
+++ 03 - developer skills and editor setup/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// ⬇️ Setting up Prettier & VS Code
 // Prettier - the best extension for formatting your code
 // custom configuration in .prettierrc file
 
@@ -11,3 +12,9 @@ const calcAge = birthYear => 2037 - birthYear;
 // global snippets file -> create snippets as you want
 
 // TODO: highlight extension - great support & customization ability
+
+// ⬇️ Installing Node.js & Setting Up a Dev Environment
+// Live Server - extension that is a muss (no need for reloading when making changes)
+// npm i live-server -> another way of running the page without need for reloading
+
+console.log(calcAge(45));
