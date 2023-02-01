fe3b7894b9d77c97faa4afa037e96248d36e2f63
im-luka <lux.9833@gmail.com>
2023-02-01 18:51:11 +0100

    05: implementing highscores

--- 05 - dom and events fundamentals/project - guess my number/script.js
+++ 05 - dom and events fundamentals/project - guess my number/script.js
@@ -17,50 +17,64 @@ console.log(document.querySelector(".guess").value);
 */
 
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highScore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector(".check").addEventListener("click", (event) => {
-  const message = document.querySelector(".message");
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess); // -> you'll get a string from input tag - casting to Number needed
 
   // When there is no guess
   if (!guess) {
-    message.textContent = "❌ No number provided";
+    document.querySelector(".message").textContent = "❌ No number provided";
 
     // When player wins the game
   } else if (guess === secretNumber) {
-    message.textContent = "🎉 Correct number!";
+    document.querySelector(".message").textContent = "🎉 Correct number!";
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
 
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
-      message.textContent = "📈 Too high!";
+      document.querySelector(".message").textContent = "📈 Too high!";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      message.textContent = "💣 You lost the game!";
+      document.querySelector(".message").textContent = "💣 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
 
     // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      message.textContent = "📈 Too low!";
+      document.querySelector(".message").textContent = "📈 Too low!";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      message.textContent = "💣 You lost the game!";
+      document.querySelector(".message").textContent = "💣 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
   }
 });
 
-// refreshing the page, you could also hard-code all of the values to it's initial values (before playing the game)
+// you could also refresh the page (but highscore would be lost) with 'window.location.reload();'
 document.querySelector(".again").addEventListener("click", () => {
-  window.location.reload();
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
