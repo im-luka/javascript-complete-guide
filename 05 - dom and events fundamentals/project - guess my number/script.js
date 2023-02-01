2e178d80ee1572fd0460d67b0c0ad0bf6f05096d
im-luka <lux.9833@gmail.com>
2023-02-01 18:26:38 +0100

    05: implementing the game logic

--- 05 - dom and events fundamentals/project - guess my number/script.js
+++ 05 - dom and events fundamentals/project - guess my number/script.js
@@ -16,11 +16,36 @@ document.querySelector(".guess").value = 17;
 console.log(document.querySelector(".guess").value);
 */
 
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", (event) => {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess); // -> you'll get a string from input tag - casting to Number needed
 
+  const message = document.querySelector(".message");
   if (!guess) {
-    document.querySelector(".message").textContent = "❌ No number provided";
+    message.textContent = "❌ No number provided";
+  } else if (guess === secretNumber) {
+    message.textContent = "🎉 Correct number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      message.textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      message.textContent = "💣 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      message.textContent = "📈 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      message.textContent = "💣 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
