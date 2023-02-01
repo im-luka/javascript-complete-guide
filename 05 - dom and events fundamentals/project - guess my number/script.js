b9ebf32ed2c98235269515751a49404594613b77
im-luka <lux.9833@gmail.com>
2023-02-01 18:12:33 +0100

    05: handling click events

--- 05 - dom and events fundamentals/project - guess my number/script.js
+++ 05 - dom and events fundamentals/project - guess my number/script.js
@@ -2,6 +2,9 @@
 
 // query selector -> select class or id in the same way
 // textContent -> text in element
+
+// Learning basics - not important
+/*
 console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "🎉 Correct number!";
@@ -11,3 +14,13 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 17;
 console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", (event) => {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess); // -> you'll get a string from input tag - casting to Number needed
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "❌ No number provided";
+  }
+});
