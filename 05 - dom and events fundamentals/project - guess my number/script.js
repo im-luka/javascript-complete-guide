aaa4cdb4da37f9ddf03e55ec6bbab87d2ddb531c
im-luka <lux.9833@gmail.com>
2023-02-01 18:03:58 +0100

    05: selecting and manipulating elements

--- 05 - dom and events fundamentals/project - guess my number/script.js
+++ 05 - dom and events fundamentals/project - guess my number/script.js
@@ -3,3 +3,11 @@
 // query selector -> select class or id in the same way
 // textContent -> text in element
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "🎉 Correct number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 17;
+console.log(document.querySelector(".guess").value);
