4db65460031ff955cca6346fa623f6c0314612f4
im-luka <lux.9833@gmail.com>
2023-02-08 21:16:27 +0100

    07: enhanced object literals

--- 07 - data structures, modern operators and strings/script.js
+++ 07 - data structures, modern operators and strings/script.js
@@ -294,3 +294,58 @@ for (const [i, el] of mainMenu.entries()) {
   console.log(`${i + 1}: ${el}`);
 }
 console.log([...mainMenu.entries()]);
+
+// ⬇️ enhanced object literals
+
+const weekdays2 = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHourse = {
+  [weekdays2[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays2[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 3}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurante = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  // enhanced object literals
+  // openingHourse: openingHourse, // instead of this
+  openingHourse, // do this
+
+  // enhanced functions!
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+console.log(restaurante);
