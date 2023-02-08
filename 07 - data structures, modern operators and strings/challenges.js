acda310381be00c4b1bcb663318cfcab9c18024c
im-luka <lux.9833@gmail.com>
2023-02-08 22:58:15 +0100

    07: challenge #3

--- 07 - data structures, modern operators and strings/challenges.js
+++ 07 - data structures, modern operators and strings/challenges.js
@@ -103,3 +103,35 @@ for (const players of game.scored.values()) {
   scorers[players] = scorers[players] ? scorers[players] + 1 : 1;
 }
 console.log(scorers);
+
+// CODING CHALLENGE #1
+console.log("----- CHALLENGE #3 -----");
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `Event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [min, event] of gameEvents) {
+  const half = `${min < 45 ? "FIRST" : "SECOND"} HALF`;
+  console.log(`[${half}] ${min}: ${event}`);
+}
