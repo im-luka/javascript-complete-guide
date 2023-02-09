6c6dcfdbf32db46dd9b92031c804853494c3ab8f
im-luka <lux.9833@gmail.com>
2023-02-09 22:27:12 +0100

    07: string methods practice

--- 07 - data structures, modern operators and strings/script.js
+++ 07 - data structures, modern operators and strings/script.js
@@ -659,6 +659,44 @@ console.log(message2.repeat(5));
 const planesInLine = (n) => {
   console.log(`There are ${n} planes in line ${"🛩️".repeat(n)}`);
 };
+
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
+
+// ⬇️ string methods practice
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsFormatted = flights.split("+");
+console.log(flightsFormatted);
+
+for (const item of flightsFormatted) {
+  const [desc, from, to, time] = item.split(";");
+
+  let str = "";
+  if (desc.includes("Delayed")) {
+    str += "🔴";
+  }
+
+  console.log(
+    str
+      .concat(
+        desc.split("_").join(" "),
+        " from ",
+        from.slice(0, 4).toUpperCase(),
+        " to ",
+        to.slice(0, 4).toUpperCase(),
+        " (",
+        time.split(":").join("h"),
+        ")"
+      )
+      .padStart(50, " ")
+  );
+}
