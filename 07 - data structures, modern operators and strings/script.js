2d387de400b417cf622dae2ad1a1ef0cfa99bca9
im-luka <lux.9833@gmail.com>
2023-02-09 21:24:59 +0100

    07: strings part 02

--- 07 - data structures, modern operators and strings/script.js
+++ 07 - data structures, modern operators and strings/script.js
@@ -560,3 +560,60 @@ checkMiddleSeat("3E");
 console.log(new String("luka")); // JS under the hood converts all strings to String Object (this)
 console.log(typeof new String("luka"));
 console.log(typeof new String("lux").slice(1));
+
+// ⬇️ strings - part 02
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = "mIChaEl";
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing email
+const email = "iamluka@mail.com";
+const loginEmail = " IamLuka@MAil.COm \n";
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim(); // you can chain methods
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+const priceGB = "288,97￡";
+const priceUS = priceGB.replace("￡", "€").replace(",", ".");
+console.log(priceUS);
+
+const announcement =
+  "All passengers come to boarding door 23. Boarding door 23!";
+// console.log(announcement.replaceAll("door", "gate"));
+console.log(announcement.replaceAll(/door/g, "gate"));
+
+// Booleans
+const newPlane = "Airbus A320neo";
+console.log(newPlane.includes("A320")); // case sensitive - of course
+console.log(newPlane.includes("Boeing"));
+console.log(newPlane.startsWith("Air"));
+
+if (newPlane.startsWith("Airbus") && newPlane.endsWith("neo")) {
+  console.log("Part of the new Airbus family!");
+}
+
+// Practice
+const checkBaggage = (items) => {
+  const baggage = items.toLowerCase(); // easily comparing
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are not allowed on board");
+  } else {
+    console.log("Welcome aboard!");
+  }
+};
+checkBaggage("I have a laptop, some foot and a pocket knife");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
