c32f14bbce342181fe6c3e8290f0f010a7309550
im-luka <lux.9833@gmail.com>
2023-02-09 23:24:51 +0100

    08: arguments - values vs reference

--- 08 - closer look at functions/script.js
+++ 08 - closer look at functions/script.js
@@ -28,3 +28,36 @@ createBooking("LH123", 2, 5);
 
 // if you want to skip parameter - just write undefined instead of it
 createBooking("LH123", undefined, 1000);
+
+// ⬇️ How Passing Arguments Works: Value vs Reference
+
+const flight = "LH234";
+const mike = {
+  name: "Mike Jordan",
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    console.log("Checked in");
+  } else {
+    console.log("Wrong passport");
+  }
+};
+
+checkIn(flight, mike);
+console.log(flight);
+console.log(mike);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = mike;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+newPassport(mike);
+checkIn(flight, mike);
