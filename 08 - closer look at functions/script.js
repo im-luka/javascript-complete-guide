6b244f896288930294588de91a5d70aea756dd93
im-luka <lux.9833@gmail.com>
2023-02-11 18:16:25 +0100

    08: closure examples

--- 08 - closer look at functions/script.js
+++ 08 - closer look at functions/script.js
@@ -248,3 +248,46 @@ booker();
 booker();
 
 console.dir(booker);
+
+// ⬇️ Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(() => {
+    console.log(`We are now boarding all ${n} passengers! 🚎`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
