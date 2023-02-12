3beecf26961a9bcd8dddc1b1433454415097b13c
im-luka <lux.9833@gmail.com>
2023-02-12 15:12:29 +0100

    09: computing usernames

--- 09 - working with arrays/script.js
+++ 09 - working with arrays/script.js
@@ -73,6 +73,18 @@ const displayMovements = (movements) => {
 };
 displayMovements(account1.movements);
 
+const createUsernames = (accounts) => {
+  accounts.forEach((acc) => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // ⬇️ Simple Array Methods
