d321a03b2ec4c2235b27aba15d83ab87562b862d
im-luka <lux.9833@gmail.com>
2023-02-12 15:03:48 +0100

    09: map method

--- 09 - working with arrays/script.js
+++ 09 - working with arrays/script.js
@@ -171,3 +171,30 @@ currenciesUnique.forEach((value, _, map) => {
 });
 
 // ⬇️ Data Transformations: '.map()', '.filter()' and '.reduce()'
+
+// ⬇️ '.map()' Method
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map((mov) => {
+  return mov * eurToUsd;
+});
+const movementsUSDarrow = movements.map((mov) => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+console.log(movementsUSDarrow);
+
+const movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementDescriptions);
