1bbacd9c2f61d6363a3cd7833fd9d84b934f4597
im-luka <lux.9833@gmail.com>
2023-02-12 15:44:36 +0100

    09: challenge #2

--- 09 - working with arrays/challenges.js
+++ 09 - working with arrays/challenges.js
@@ -19,3 +19,19 @@ const katesData = [10, 5, 6, 1, 4];
     console.log(`Dog number ${i + 1} is ${ageLabel}`);
   });
 })(juliasData, katesData);
+
+// CODING CHALLENGE #2
+console.log("----- CHALLENGE #2 -----");
+
+const calcAverageHumanAge = (dogs) => {
+  const humanDogs = dogs.map((dog) => (dog <= 2 ? dog * 2 : 16 + dog * 4));
+  const adultDogs = humanDogs.filter((dog) => dog > 18);
+  // const average = adultDogs.reduce((acc, dog) => acc + dog, 0) / adultDogs.length;
+  const average = adultDogs.reduce(
+    (acc, dog, _, arr) => acc + dog / arr.length,
+    0
+  );
+  return average;
+};
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
