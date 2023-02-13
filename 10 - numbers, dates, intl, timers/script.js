f228768488b788fad67d7d5b12d527c86cbbf811
im-luka <lux.9833@gmail.com>
2023-02-13 21:51:05 +0100

    10: creating dates

--- 10 - numbers, dates, intl, timers/script.js
+++ 10 - numbers, dates, intl, timers/script.js
@@ -406,3 +406,40 @@ console.log(huge + " is REALLY big!!!");
 console.log(10n / 3n);
 console.log(11n / 3n);
 console.log(10 / 3);
+
+// ⬇️ Creating Dates
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date("Feb 13 2023 21:44:10"));
+console.log(new Date("December 25, 2015")); // could be unreliable, bad practice
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31)); // js will correct the date
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+console.log("-------------------------");
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
