c5c00984507b6fcd184e3a17e2c7e1fee3dd1b69
im-luka <lux.9833@gmail.com>
2023-02-16 21:29:40 +0100

    11: selecting, creating and deleting elements

--- 11 - advanced dom and events/script.js
+++ 11 - advanced dom and events/script.js
@@ -29,3 +29,46 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ⬇️ Selecting, Creating and Deleting Elements
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections); // NodeList
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons); // HTMLCollection -> if element changes, DOM is updated automatically
+
+console.log(document.getElementsByClassName("btn")); // HTMLCollection
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "We use cookies for improved functionality and analytics."
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// element can only be added once (prepend or append)
+header.prepend(message); // add as first child
+header.append(message); // add as last child
+// '.closeNode(true)' method must be used to clone the element and append it more than once
+// header.append(message.cloneNode(true));
+
+header.before(message); // before the parent
+header.after(message); // after the parent
+
+header.append(message);
+
+// Delete elements
+document.querySelector(".btn--close-cookie").addEventListener("click", () => {
+  message.remove(); // remove element from DOM
+  message.parentElement.removeChild(message); // the old way of removing - outdated
+});
