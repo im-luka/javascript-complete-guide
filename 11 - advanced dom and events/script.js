742b1381c27cb1eaa7613d629472e6b5596bd69d
im-luka <lux.9833@gmail.com>
2023-03-05 16:20:16 +0100

    11: event propagation

--- 11 - advanced dom and events/script.js
+++ 11 - advanced dom and events/script.js
@@ -177,3 +177,33 @@ setTimeout(() => {
 }, 3000);
 
 // ⬇️ Event Propagation: Bubbling and Capturing
+
+// ⬇️ Event Propagation
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation(); // -> not good idea in practice
+});
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("CONTAINER", e.target, e.currentTarget);
+});
+
+document.querySelector(".nav").addEventListener(
+  "click",
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("NAV", e.target, e.currentTarget);
+  },
+  true
+);
