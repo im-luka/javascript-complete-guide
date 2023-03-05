4014ac5f78204eb5e97e3a0943f91a4f11788a68
im-luka <lux.9833@gmail.com>
2023-03-05 15:57:24 +0100

    11: types of events and event handlers

--- 11 - advanced dom and events/script.js
+++ 11 - advanced dom and events/script.js
@@ -159,3 +159,19 @@ btnScrollTo.addEventListener("click", (event) => {
 
   section1.scrollIntoView({ behavior: "smooth" }); // modern and easier way
 });
+
+// ⬇️ Types of Events and Event Handlers
+
+const h1 = document.querySelector("h1");
+
+const alertH1 = (e) => {
+  alert("addEventListener: Great! You are reading the heading");
+};
+
+// h1.onmouseenter = alertH1;
+
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener("mouseenter", alertH1);
+}, 3000);
