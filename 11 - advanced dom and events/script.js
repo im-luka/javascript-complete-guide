c05ed38f9e775fb984bf99d4a2568b1fc6009405
im-luka <lux.9833@gmail.com>
2023-03-05 16:44:39 +0100

    11: dom traversing

--- 11 - advanced dom and events/script.js
+++ 11 - advanced dom and events/script.js
@@ -165,7 +165,7 @@ btnScrollTo.addEventListener("click", (event) => {
 const h1 = document.querySelector("h1");
 
 const alertH1 = (e) => {
-  alert("addEventListener: Great! You are reading the heading");
+  // alert("addEventListener: Great! You are reading the heading");
 };
 
 // h1.onmouseenter = alertH1;
@@ -227,5 +227,37 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// ⬇️ DOM Traversing
+
+// const h1 = document.querySelector("h1");
+
+// Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "unset"; // var(--gradient-secondary)
+h1.closest("h1").style.background = "unset"; // var(--gradient-primary)
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transform = "scale(1)"; // scale(0.5)
   }
 });
