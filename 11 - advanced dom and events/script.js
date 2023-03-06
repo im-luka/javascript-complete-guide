174a06bc7af72798de3ed766ec13e5a520a19448
im-luka <lux.9833@gmail.com>
2023-03-06 21:13:38 +0100

    11: revealing elements on scroll

--- 11 - advanced dom and events/script.js
+++ 11 - advanced dom and events/script.js
@@ -360,3 +360,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// ⬇️ Revealing Elements on Scroll
+
+// const allSections = document.querySelectorAll(".section")
+
+const revealSections = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    return;
+  }
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
