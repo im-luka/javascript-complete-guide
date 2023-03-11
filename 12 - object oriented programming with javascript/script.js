4d619a65ff4cb386ca9628ca4e2af557d93f6675
im-luka <lux.9833@gmail.com>
2023-03-11 12:40:44 +0100

    12: object.create() method

--- 12 - object oriented programming with javascript/script.js
+++ 12 - object oriented programming with javascript/script.js
@@ -173,3 +173,28 @@ class HouseCl {
 const house2 = new HouseCl("Rijeka", "gray");
 
 HouseCl.size();
+
+// ⬇️ Object.create()
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = "Steven";
+steven.birthYear = 1993;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init("Sarah", 1979);
+sarah.calcAge();
