88ac4ac2e8377c4a4feabd9c847bdcacd70102e9
im-luka <lux.9833@gmail.com>
2023-03-11 12:17:07 +0100

    12: es6 classes

--- 12 - object oriented programming with javascript/script.js
+++ 12 - object oriented programming with javascript/script.js
@@ -78,3 +78,37 @@ console.log(arr.unique());
 const h1 = document.querySelector("h1");
 console.dir(h1);
 console.dir((x) => x + 1);
+
+// ⬇️ ES6 Classes
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// also working as always
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+jessica.greet();
+
+// 1. classes are NOT hoisted
+// 2. classes are first-class citizens
+// 3. classes are executed in strict mode
