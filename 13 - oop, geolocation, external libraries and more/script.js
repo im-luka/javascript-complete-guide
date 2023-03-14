e590f2b98089a3cc48ac748e1b596cea37e2f261
im-luka <lux.9833@gmail.com>
2023-03-14 18:03:31 +0100

    13: displaying a map marker

--- 13 - oop, geolocation, external libraries and more/script.js
+++ 13 - oop, geolocation, external libraries and more/script.js
@@ -37,6 +37,7 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 // ⬇️ Using the Geolocation API
 // ⬇️ Displaying a Map Using Leaflet Library
+// ⬇️ Displaying a Map Marker
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -45,16 +46,31 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       const map = L.map("map").setView(coords, 13);
+      // console.log(map);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", (mapEvent) => {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     () => alert("Could not get your position.")
   );
