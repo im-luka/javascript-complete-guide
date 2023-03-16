2004d7f8329656e1d810e2f10f2d05853a74bc21
im-luka <lux.9833@gmail.com>
2023-03-16 19:33:52 +0100

    14: coding challenge 02

--- 14 - async js - promises, async-await, ajax/challenges.js
+++ 14 - async js - promises, async-await, ajax/challenges.js
@@ -2,7 +2,7 @@
 
 // CODING CHALLENGE #1
 
-const whereAmI = (lat, lng) => {
+const whereAmINow = (lat, lng) => {
   const url = `https://geocode.xyz/${lat},${lng}?geoit=json`;
   fetch(url)
     .then((response) => {
@@ -29,4 +29,50 @@ const whereAmI = (lat, lng) => {
     .catch((err) => console.log(`${err.message} 💥`));
 };
 
-whereAmI(29, 31);
+whereAmINow(29, 31);
+
+// CODING CHALLENGE #2
+
+function createImage(imgPath) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", (e) => {
+      document.querySelector(".images").append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", (e) => {
+      reject(new Error("image not found"));
+    });
+  });
+}
+
+let currImg;
+
+createImage("img/img-1.jpg")
+  .then((img) => {
+    currImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then((img) => {
+    currImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currImg.style.display = "none";
+    return createImage("img/img-3.jpg");
+  })
+  .then((img) => {
+    currImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currImg.style.display = "none";
+  })
+  .catch((err) => console.error(err));
