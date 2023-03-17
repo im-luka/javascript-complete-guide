6cda17f42783547dad235552bed465c00fd4a1d8
im-luka <lux.9833@gmail.com>
2023-03-17 21:02:57 +0100

    14: running promises in parallel

--- 14 - async js - promises, async-await, ajax/script.js
+++ 14 - async js - promises, async-await, ajax/script.js
@@ -470,3 +470,26 @@ console.log("1. Will get location");
     console.error("2.", error);
   }
 })();
+
+// ⬇️ Running Promises in Parallel
+
+const get3Countries = async (c1, c2, c3) => {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+    // console.log(data);
+
+    console.log(data.map((d) => d[0].capital));
+  } catch (error) {
+    console.error(error);
+  }
+};
+get3Countries("croatia", "portugal", "spain");
