96f04513ddee9fbb42fe1861205e91c33121b8c7
im-luka <lux.9833@gmail.com>
2023-03-17 22:28:17 +0100

    15: the module pattern

--- 15 - modern javascript - modules, tooling and functional/script.js
+++ 15 - modern javascript - modules, tooling and functional/script.js
@@ -62,3 +62,35 @@ lastPost.then((last) => console.log(last));
 // better way
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+// ⬇️ The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = (product, quantity) => {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); // undefined
