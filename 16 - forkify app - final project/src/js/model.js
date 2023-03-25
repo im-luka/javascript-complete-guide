73515ae124aa8cd8d5e7c25a2286fbd76558d402
im-luka <lux.9833@gmail.com>
2023-03-25 11:11:06 +0100

    16: implementing bookmarks part 01

--- 16 - forkify app - final project/src/js/model.js
+++ 16 - forkify app - final project/src/js/model.js
@@ -11,6 +11,7 @@ export const state = {
     page: 1,
     resultsPerPage: RES_PER_PAGE,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -28,6 +29,11 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+    if (state.bookmarks.some((bookmark) => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else {
+      state.recipe.bookmarked = false;
+    }
   } catch (error) {
     throw error;
   }
@@ -44,6 +50,7 @@ export const loadSearchResults = async function (query) {
       publisher: recipe.publisher,
       image: recipe.image_url,
     }));
+    state.search.page = 1;
   } catch (error) {
     throw error;
   }
@@ -61,4 +68,25 @@ export const updateServings = function (newServings) {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
   });
   state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // add bookmark
+  state.bookmarks.push(recipe);
+
+  // mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) {
+    state.recipe.bookmarked = true;
+  }
+};
+
+export const deleteBookmark = function (id) {
+  // delete bookmark
+  const index = state.bookmarks.findIndex((el) => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // mark current recipe as not bookmarked
+  if (id === state.recipe.id) {
+    state.recipe.bookmarked = false;
+  }
 };
